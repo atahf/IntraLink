0482d3b5af81b64b3a7514611fae69c8e41e8708
atahf <ataollah@sabanciuniv.edu>
2023-08-29 19:15:51 +0300

    fixed small issue #2

--- src/main/java/com/atahf/IntraLink/firebase/FirebaseConfig.java
+++ src/main/java/com/atahf/IntraLink/firebase/FirebaseConfig.java
@@ -3,9 +3,10 @@ package com.atahf.IntraLink.firebase;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -13,18 +14,13 @@ import java.nio.charset.StandardCharsets;
 @Configuration
 public class FirebaseConfig {
 
-    private final FirebaseProperties firebaseProperties;
-
-    @Autowired
-    public FirebaseConfig(FirebaseProperties firebaseProperties) {
-        this.firebaseProperties = firebaseProperties;
-    }
+    private static String credentials = System.getenv("FIREBASE_CREDENTIALS");
 
     @Bean
     public FirebaseApp initializeFirebaseApp() throws IOException {
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(
-                        new ByteArrayInputStream(firebaseProperties.getCredentials().getBytes(StandardCharsets.UTF_8))))
+                        new ByteArrayInputStream(credentials.getBytes(StandardCharsets.UTF_8))))
                 .setStorageBucket("intralink-94cfb.appspot.com")
                 .build();
 
