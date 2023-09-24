df905ff3acc818ccb20040329e7607353ca3c8b9
atahf <ataollah@sabanciuniv.edu>
2023-09-24 21:42:58 +0300

    minor fixes

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Card, Row, Col } from 'react-bootstrap';
+import { Card, Row, Col, Container } from 'react-bootstrap';
 import UserProfileAvatar from '../assets/user.jpg';
 
 const User = ({ userData }) => {
@@ -29,28 +29,35 @@ const User = ({ userData }) => {
     }, []);
 
     return (
-        <Row>
-            <Col xs={6} md={4}>
-                <Card.Img variant="top" src={picture ? picture : UserProfileAvatar} alt='UserProfileAvatar' />
-            </Col>
-            <Col xs={12} md={8}>
-                <Card.Body>
-                    <Card.Text style={{ padding: '0px 40px' }}>
-                        Name: <b>{capitalizeWords(userData.firstName) + ' ' + capitalizeWords(userData.lastName)}</b> <br/>
-                        Username: <b>{ userData.username }</b> <br/>
-                        E-mail: <b>{userData.email}</b> <br/>
-                        Role: <b>{userData.role}</b> <br/>
-                        Department: <b>{userData.department}</b> <br/>
-                        Birthdate: <b>{userData.birthdate}</b> <br/>
-                        Age: <b>{userData.age}</b> <br/>
-                        Gender: <b>{userData.gender}</b> <br/>
-                        Birthdate: <b>{userData.birthdate}</b> <br/>
-                        Phone Number: <b>{userData.phoneNumber}</b> <br/>
-                        Address: <b>{userData.address}</b>
-                    </Card.Text>
-                </Card.Body>
-            </Col>
-        </Row>
+        <Container style={{padding: '15px', margin: '5px auto'}}>
+            <Row>
+                <Col xs={6} md={4} className="d-flex justify-content-center align-items-center">
+                    <Card.Img 
+                        variant="top" 
+                        src={picture ? picture : UserProfileAvatar} 
+                        alt='UserProfileAvatar'
+                        style={{width: '300px', height: 'auto'}}
+                    />
+                </Col>
+                <Col xs={12} md={8} className="d-flex justify-content-center align-items-center">
+                    <Card.Body>
+                        <Card.Text style={{ padding: '0px 40px' }}>
+                            Name: <b>{capitalizeWords(userData.firstName) + ' ' + capitalizeWords(userData.lastName)}</b> <br/>
+                            Username: <b>{ userData.username }</b> <br/>
+                            E-mail: <b>{userData.email}</b> <br/>
+                            Role: <b>{userData.role}</b> <br/>
+                            Department: <b>{userData.department}</b> <br/>
+                            Birthdate: <b>{userData.birthdate}</b> <br/>
+                            Age: <b>{userData.age}</b> <br/>
+                            Gender: <b>{userData.gender}</b> <br/>
+                            Birthdate: <b>{userData.birthdate}</b> <br/>
+                            Phone Number: <b>{userData.phoneNumber}</b> <br/>
+                            Address: <b>{userData.address}</b>
+                        </Card.Text>
+                    </Card.Body>
+                </Col>
+            </Row>
+        </Container>
     );
 }
  
--- src/routes/Users.js
+++ src/routes/Users.js
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 const Users = () => {
     return (
         <div className='users-page'>
-            <UsersBox/>
+            <UsersBox style={{width: '1000px'}}/>
         </div>
     );
 }
