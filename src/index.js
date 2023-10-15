a9d9e5aa8882b39a14240a3df1e24ab61da78363
atahf <ataollah@sabanciuniv.edu>
2023-10-15 16:46:00 +0300

    better routing

--- src/index.js
+++ src/index.js
@@ -2,17 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 
 import { AuthContextProvider } from './context/AuthContext';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 
+const router = createHashRouter([
+	{
+	  path: "/*",
+	  element: <App />,
+	}
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<AuthContextProvider>
-			<App />
+			<RouterProvider router={router} />
 		</AuthContextProvider>
 	</React.StrictMode>
 );
